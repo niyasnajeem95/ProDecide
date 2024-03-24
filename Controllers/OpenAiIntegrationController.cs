e3a18859c9b1e29c973fb22a5d8411af72ef60be
niyasnajeem <niyasnajeem666@gmail.com>
2024-03-24 11:13:09 +0530

    CONFIG

--- Controllers/OpenAiIntegrationController.cs
+++ Controllers/OpenAiIntegrationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Net;
 
 namespace ProDecide.Controllers
@@ -9,17 +10,19 @@ namespace ProDecide.Controllers
     {
       
         private readonly ILogger<OpenAiIntegrationController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public OpenAiIntegrationController(ILogger<OpenAiIntegrationController> logger)
+        public OpenAiIntegrationController(ILogger<OpenAiIntegrationController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpPost(Name = "GetWeatherForecast")]
         public async Task<string> Post([FromBody] string query)
         {
 
-            string apiKey = Environment.GetEnvironmentVariable("API_KEY");
+            string apiKey = _configuration.GetSection("API_KEY").Value; 
             var openaiService = new OpenAIService(apiKey);
 
             // Get response from OpenAI API
